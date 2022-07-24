78a33efb83a8627c9beff92b390fffe37916906e
Michael Katz <mikekatz04@gmail.com>
2022-07-25 01:59:00 +0200

    updates

--- README.md
+++ README.md
@@ -8,12 +8,22 @@ The only prerequisite is `conda`.
 	cd LDC-waveform-generation-tutorial
 	```
 
-2. To run install:
+2. Setup the `conda environment`:
+	```
+	conda create -n tutor_env -c conda-forge clang_osx-64 clangxx_osx-64 wget gsl fftw lapack=3.6.1 hdf5 numpy Cython scipy jupyter ipython  matplotlib python=3.7 --yes
+	```
+
+3. Activate the environment:
+	```
+	conda activate tutor_env
+	```
+
+4. To run install:
 	```
 	bash setup.sh
 	```
 
-3. Open the notebook:
+5. Open the notebook:
 	```
 	jupyter notebook Waveform\ Generation.ipynb
 	```
--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-#conda create -n tutor_env -c conda-forge clang_osx-64 clangxx_osx-64 wget gsl fftw lapack=3.6.1 hdf5 numpy Cython scipy jupyter ipython  matplotlib python=3.7 --yes
-#conda activate tutor_env
 pip install tqdm h5py requests astropy h5py lisaconstants xarray
 pip install git+https://tutorials:Z8uyiSN5H4v45jmhyWHd@gitlab.in2p3.fr/LISA/LDC --install-option '--with-fastGB' --install-option '--with-imrphenomD'
 wget -q --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sKUqLcC83o3OFCmVj5AyXFW6pn2EEmdH' -O lisabeta_release.zip
